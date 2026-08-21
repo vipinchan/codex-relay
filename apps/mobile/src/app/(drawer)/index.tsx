1ae4fb327679bb79cf450418e94c0508dd321b83
VipinChan <vipinchan@hotmail.com>
2026-08-22 00:30:07 +0800

    feat(mobile): make conversation history the home screen

--- apps/mobile/src/app/(drawer)/index.tsx
+++ apps/mobile/src/app/(drawer)/index.tsx
@@ -1,5 +1,16 @@
+import { useSelector } from "@legendapp/state/react";
+
 import { ChatScreen } from "@/components/chat/ChatScreen";
+import { ConversationHistoryScreen } from "@/components/chat/ConversationHistoryScreen";
+import { chatStore$ } from "@/state/chat-store";
 
 export default function HomeScreen() {
+  const activeThreadId = useSelector(() => chatStore$.activeThreadId.get());
+  const hasPairedSession = useSelector(() => chatStore$.hasPairedSession.get());
+
+  if (hasPairedSession && !activeThreadId) {
+    return <ConversationHistoryScreen />;
+  }
+
   return <ChatScreen />;
 }
